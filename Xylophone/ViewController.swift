6a96c30a9b901f71908d8065cf5bc9cc578d23f5
Mehmet Emrah Konya <mehmetemrahk@gmail.com>
2024-05-17 11:26:50 +0300

    Add play sound function

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,35 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
 
+    @IBAction func notePressed(_ sender: UIButton) {
+        if let noteName = sender.currentTitle{
+            print(noteName)
+            playSound(tune: noteName)
+            
+        }
+    }
     
+    func playSound(tune:String){
+        print("\(tune) played")
+        guard let path = Bundle.main.url(forResource: tune, withExtension: "wav") else {return}
+        do {
+            player = try AVAudioPlayer(contentsOf: path)
+            player?.play()
+        } catch let error{
+            print(error.localizedDescription)
+        }
+    }
 
 }
 
